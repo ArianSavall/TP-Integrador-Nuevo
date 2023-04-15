d28c7a5913c9fdf400755a1851ac626c8a13bac9
savallarian45@gmail.com <savallarian45@gmail.com>
2023-04-15 19:03:29 -0300

    SQL en proceso

---
+++ src/main/java/org/utilities/Conector_SQL.java
@@ -0,0 +1,10 @@
+package org.utilities;
+
+public class Conector_SQL {
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    public static final String DB_URL = "jdbc:mysql://localhost:3306/nuevo_tp_integrador";
+
+    // Credenciales credentials
+    public static final String USER = "root";
+    public static final String PASS = "1234";
+}
--- src/main/java/org/utilities/LectordeArchivos.java
+++ src/main/java/org/utilities/LectordeArchivos.java
@@ -5,22 +5,22 @@ import org.Modelos.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.utilities.Conector_SQL.*;
+
 public class LectordeArchivos {
-    private Path rutaResultados;
-    private Path rutaPronostico;
+
+    Connection conexion = null;
+    Statement consulta = null;
+
     private List<Ronda> rondas = new ArrayList<>();
     private List<Equipo> equipos = new ArrayList<>();
     private List<Pronostico> pronosticos = new ArrayList<>();
     private List<Persona> personas = new ArrayList<>();
 
-
-    public LectordeArchivos(Path rutaResultados, Path rutaPronostico) {
-        this.rutaResultados = rutaResultados;
-        this.rutaPronostico = rutaPronostico;
-    }
 
     //metodos para buscar
     public Ronda buscarRonda(Integer i) {
@@ -51,158 +51,187 @@ public class LectordeArchivos {
 
 
     public void leerResultados(){
-        List<String> lineasResultado = new ArrayList<>();
+
         try {
-            lineasResultado = Files.readAllLines(rutaResultados);
-        } catch (
-                IOException e) {
-            System.out.println("No se pudo leer la linea de resultados...");
-            System.out.println(e.getMessage());
-            System.exit(1);
-        }
-        boolean primera = true;
-        for (String lineaResultado : lineasResultado) {
-            if (primera) {
-                primera = false;
-            } else {
-                String[] campos = lineaResultado.split(";");
-                Equipo equipo1 = this.buscarEquipo(campos[1]);
-                Equipo equipo2 = this.buscarEquipo(campos[4]);
+            //abrir conexion
+            conexion = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            //Ejecutar consulta
+            consulta = conexion.createStatement();
+            String sql;
+            sql = "SELECT Ronda, Equipo 1, Cant. goles 1, Cant. goles 2, Equipo 2 FROM nuevo_tp_integrador.resultados";
+
+            ResultSet resultado = consulta.executeQuery(sql);
+
+            while (resultado.next()) {
+
+                int rondaDB = resultado.getInt("Ronda");
+                String equipo1DB = resultado.getString("Equipo 1");
+                int cantgoles1DB = resultado.getInt("Cant. goles 1");
+                int cantgoles2DB = resultado.getInt("Cant. goles 2");
+                String equipo2DB = resultado.getString("Equipo 2");
+
+                System.out.println(rondaDB + equipo1DB + cantgoles1DB + cantgoles2DB + equipo2DB);
+
+                Equipo equipo1 = this.buscarEquipo(equipo1DB);
+                Equipo equipo2 = this.buscarEquipo(equipo2DB);
 
                 //Si el equipo no existe, instanciar uno nuevo
                 if (equipo1 == null) {
-                    equipo1 = new Equipo(campos[1]);
+                    equipo1 = new Equipo(equipo1DB);
                     equipos.add(equipo1);
                 }
                 if (equipo2 == null) {
-                    equipo2 = new Equipo(campos[4]);
+                    equipo2 = new Equipo(equipo2DB);
                     equipos.add(equipo2);
                 }
 
 
                 for (Equipo equipo : this.equipos) {
-                    if (equipo.getNombre().equals(campos[1])) {
+                    if (equipo.getNombre().equals(equipo1DB)) {
                         equipo1 = equipo;
                     }
 
-                    if (equipo.getNombre().equals(campos[4])) {
+                    if (equipo.getNombre().equals(equipo2DB)) {
                         equipo2 = equipo;
                     }
                 }
 
                 //instancio un nuevo partido
                 Partido partido = new Partido(equipo1, equipo2);
-                partido.setCantGoles1(Integer.parseInt(campos[2]));
-                partido.setCantGoles2(Integer.parseInt(campos[3]));
+                partido.setCantGoles1(cantgoles1DB);
+                partido.setCantGoles2(cantgoles2DB);
 
-                Ronda ronda = this.buscarRonda(Integer.parseInt(campos[0]));
+                Ronda ronda = this.buscarRonda(rondaDB);
 
                 //Si no existe una ronda, crearla
                 if (ronda != null) {
                     ronda.agregarPartidos(partido);
 
                 } else {
-                    ronda = new Ronda(Integer.parseInt(campos[0]));
+                    ronda = new Ronda(rondaDB);
                     ronda.agregarPartidos(partido);
                     rondas.add(ronda);
                 }
             }
+            // Esto se utiliza par cerrar la conexión con la base de datos
+            resultado.close();
+            consulta.close();
+            conexion.close();
+        } catch (SQLException se) {
+            // Execpción ante problemas de conexión
+            se.printStackTrace();
         }
-    }
-
-    public void leerPronosticos() {
-        List<String> lineasPronostico = new ArrayList<>();
-        int puntos = 0;
-
-        Ronda rondapatron = new Ronda(1);
-        try {
-            lineasPronostico = Files.readAllLines(rutaPronostico);
-        } catch (IOException e) {
-            System.out.println("No se pudo leer la linea de pronosticos...");
-            System.out.println(e.getMessage());
-            System.exit(1);
-        }
-        boolean primera = true;
-        for (String lineaPronostico : lineasPronostico) {
-            if (primera) {
-                primera = false;
-            } else {
-                String[] campos = lineaPronostico.split(";");
-                Equipo equipo1 = this.buscarEquipo(campos[2]);
-                Equipo equipo2 = this.buscarEquipo(campos[6]);
-
-
-                if (equipo1!=null){
-                    equipo1 = new Equipo(campos[2]);
-                }
-                if (equipo2!=null){
-                    equipo2 = new Equipo(campos[6]);
-                }
-
-                Ronda ronda = new Ronda(Integer.parseInt(campos[0]));
-
-                for (Ronda rondaLista : rondas) {
-                    if (rondaLista.getNro().equals(ronda.getNro())){ //si la ronda que está en la lista es igual a la ronda que lee en el pronostico...
-                        ronda=rondaLista;
-                    }
-                }
-
-                if(rondapatron.getNro()!=ronda.getNro()){ //si se detecta un cambio de ronda, que imprima los puntajes de la ronda que acaba de finalizar
-                    System.out.println("Resultados de la ronda " + rondapatron.getNro());
-                    for(int i=0; i<this.personas.size(); i++) {
-                     System.out.println(personas.get(i).getNombre());
-                     System.out.println(personas.get(i).getPuntaje());
-                    }
-                    rondapatron.setNro(ronda.getNro());
-                }
-
-                Persona persona = this.buscarPersona(campos[1]);
-
-                if(persona==null){ //si no existe la persona, crearla, añadirla a la lista y reiniciar la variable puntos
-                   persona= new Persona(campos[1], ronda.getNro());
-                   this.personas.add(persona);
-                   puntos=0;
-                }
-
-                if(persona.getNroRonda()!=ronda.getNro()){ //si la persona ya existe pero cambió la ronda, que se reinicien los puntos
-                    persona.setNroRonda(ronda.getNro());
-                    puntos = 0;
-                }
-
-                Partido partido = ronda.buscarPartido(equipo1, equipo2);
-                Equipo equipoPred = null;
-                ResultadoEnum resultadoPred = null;
-
-                if("X".equals(campos[3])){  //si la X está en gana1
-                    equipoPred = partido.getEquipo1();
-                    resultadoPred = ResultadoEnum.GANADOR;
-
-                }
-                if("X".equals(campos[4])){  //si la X está en empate
-                    equipoPred = partido.getEquipo1();
-                    resultadoPred = ResultadoEnum.EMPATE;
-                }
-
-                if("X".equals(campos[5])){  //si la X está en gana2
-                    equipoPred = partido.getEquipo1();
-                    resultadoPred = ResultadoEnum.PERDEDOR;
-                }
-
-                Pronostico pronostico = new Pronostico(partido, equipoPred, persona);
-
-                //sumamos los puntos que correspondan y los metemos en el puntaje de la persona
-                puntos += pronostico.puntos(resultadoPred);
-                pronostico.getPersona().setPuntaje(puntos);
-
-                this.pronosticos.add(pronostico);
+        finally {
+            // Esta sentencia es para que ante un problema con la base igual se cierren las conexiones
+            try {
+                if (consulta != null)
+                    consulta.close();
+            } catch (SQLException se2) {
+            }
+            try {
+                if (conexion != null)
+                    conexion.close();
+            } catch (SQLException se) {
+                se.printStackTrace();
             }
         }
-
-        //imprimo los resultados de la ronda final
-        System.out.println("Resultados de la ronda " + rondapatron.getNro());
-        for(int k=0; k<this.personas.size(); k++) {
-            System.out.println(personas.get(k).getNombre());
-            System.out.println(personas.get(k).getPuntaje());
-        }
     }
+
+
+//    public void leerPronosticos() {
+//        List<String> lineasPronostico = new ArrayList<>();
+//        int puntos = 0;
+//
+//        Ronda rondapatron = new Ronda(1);
+//        try {
+//            lineasPronostico = Files.readAllLines(rutaPronostico);
+//        } catch (IOException e) {
+//            System.out.println("No se pudo leer la linea de pronosticos...");
+//            System.out.println(e.getMessage());
+//            System.exit(1);
+//        }
+//        boolean primera = true;
+//        for (String lineaPronostico : lineasPronostico) {
+//            if (primera) {
+//                primera = false;
+//            } else {
+//                String[] campos = lineaPronostico.split(";");
+//                Equipo equipo1 = this.buscarEquipo(campos[2]);
+//                Equipo equipo2 = this.buscarEquipo(campos[6]);
+//
+//
+//                if (equipo1!=null){
+//                    equipo1 = new Equipo(campos[2]);
+//                }
+//                if (equipo2!=null){
+//                    equipo2 = new Equipo(campos[6]);
+//                }
+//
+//                Ronda ronda = new Ronda(Integer.parseInt(campos[0]));
+//
+//                for (Ronda rondaLista : rondas) {
+//                    if (rondaLista.getNro().equals(ronda.getNro())){ //si la ronda que está en la lista es igual a la ronda que lee en el pronostico...
+//                        ronda=rondaLista;
+//                    }
+//                }
+//
+//                if(rondapatron.getNro()!=ronda.getNro()){ //si se detecta un cambio de ronda, que imprima los puntajes de la ronda que acaba de finalizar
+//                    System.out.println("Resultados de la ronda " + rondapatron.getNro());
+//                    for(int i=0; i<this.personas.size(); i++) {
+//                     System.out.println(personas.get(i).getNombre());
+//                     System.out.println(personas.get(i).getPuntaje());
+//                    }
+//                    rondapatron.setNro(ronda.getNro());
+//                }
+//
+//                Persona persona = this.buscarPersona(campos[1]);
+//
+//                if(persona==null){ //si no existe la persona, crearla, añadirla a la lista y reiniciar la variable puntos
+//                   persona= new Persona(campos[1], ronda.getNro());
+//                   this.personas.add(persona);
+//                   puntos=0;
+//                }
+//
+//                if(persona.getNroRonda()!=ronda.getNro()){ //si la persona ya existe pero cambió la ronda, que se reinicien los puntos
+//                    persona.setNroRonda(ronda.getNro());
+//                    puntos = 0;
+//                }
+//
+//                Partido partido = ronda.buscarPartido(equipo1, equipo2);
+//                Equipo equipoPred = null;
+//                ResultadoEnum resultadoPred = null;
+//
+//                if("X".equals(campos[3])){  //si la X está en gana1
+//                    equipoPred = partido.getEquipo1();
+//                    resultadoPred = ResultadoEnum.GANADOR;
+//
+//                }
+//                if("X".equals(campos[4])){  //si la X está en empate
+//                    equipoPred = partido.getEquipo1();
+//                    resultadoPred = ResultadoEnum.EMPATE;
+//                }
+//
+//                if("X".equals(campos[5])){  //si la X está en gana2
+//                    equipoPred = partido.getEquipo1();
+//                    resultadoPred = ResultadoEnum.PERDEDOR;
+//                }
+//
+//                Pronostico pronostico = new Pronostico(partido, equipoPred, persona);
+//
+//                //sumamos los puntos que correspondan y los metemos en el puntaje de la persona
+//                puntos += pronostico.puntos(resultadoPred);
+//                pronostico.getPersona().setPuntaje(puntos);
+//
+//                this.pronosticos.add(pronostico);
+//            }
+//        }
+//
+//        //imprimo los resultados de la ronda final
+//        System.out.println("Resultados de la ronda " + rondapatron.getNro());
+//        for(int k=0; k<this.personas.size(); k++) {
+//            System.out.println(personas.get(k).getNombre());
+//            System.out.println(personas.get(k).getPuntaje());
+//        }
+//    }
 }
